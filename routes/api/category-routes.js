70eb9374757b29e7313a9c479ae747e348b24447
brandonlambrecht <brandonjlambrecht@gmail.com>
2024-01-12 20:49:29 -0600

    update category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,109 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
-// The `/api/categories` endpoint
+// Route api/categories
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+// Get all categories
+router.get("/", async (req, res) => {
+  try {
+    const getAllCategories = await Category.findAll({
+      include: [{ model: Product }],
+    });
+
+    if (getAllCategories.length === 0) {
+      return res.status(404).json({ message: "No categories found" });
+    }
+
+    return res.status(200).json({
+      getAllCategories,
+      message: "Categories found successfully",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+// Get categories by ID
+router.get("/:id", async (req, res) => {
+  try {
+    const getSingleCategory = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!getSingleCategory) {
+      return res.status(404).json({ message: "No categories found by ID" });
+    }
+
+    return res.status(200).json({
+      getSingleCategory,
+      message: "Category found by ID successfully",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+// Create category
+router.post("/", async (req, res) => {
+  try {
+    const createCategory = await Category.create(req.body);
+
+    if (!createCategory) {
+      return res.status(404).json({ message: "No category created" });
+    }
+
+    return res.status(200).json({
+      createCategory,
+      message: "Category created successfully",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+// Update category
+router.put("/:id", async (req, res) => {
+  try {
+    const createCategory = await Category.update(req.body, {
+      where: { id: req.params.id },
+    });
+
+    if (!createCategory) {
+      return res.status(404).json({ message: "No category found by ID" });
+    }
+
+    return res.status(200).json({
+      createCategory,
+      message: "Category created successfully",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+// Delete category by ID
+router.delete("/:id", async (req, res) => {
+  try {
+    const deleteCategory = await Category.findOneAndRemove(req.body, {
+      where: { id: req.params.id },
+    });
+
+    if (!deleteCategory) {
+      return res.status(404).json({ message: "Failed to delete category" });
+    }
+
+    return res.status(200).json({
+      deleteCategory,
+      message: "Category deleted successfully",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 module.exports = router;
